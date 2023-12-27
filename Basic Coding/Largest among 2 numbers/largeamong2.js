e1a5fb3bf4364c32ec25a2e72035b477fe7f9561
shovankarmakar1911 <shovankarmakar1911@gmail.com>
2023-12-27 23:02:45 +0530

    Largest among 2 numbers

---
+++ Basic Coding/Largest among 2 numbers/largeamong2.js
@@ -0,0 +1,19 @@
+/* Write a program to print the largest number of the two numbers given.
+
+
+
+Input:-
+
+a=3, b=4
+
+Output:
+
+4 */
+
+let a = 3;
+let b = 4;
+if (a > b) {
+  console.log(a);
+} else {
+  console.log(b);
+}
