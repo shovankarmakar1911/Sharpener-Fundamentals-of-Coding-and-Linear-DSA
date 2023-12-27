e951bac63caf918191fb08ddca4e3cc478a85853
shovankarmakar1911 <shovankarmakar1911@gmail.com>
2023-12-27 22:57:52 +0530

    Swap Two Variables a and b

---
+++ Basic Coding/Swap Two Variables a and b/swap.js
@@ -0,0 +1,21 @@
+/* Write a program to Swap two Variables a and b (Swapping basically means interchanging)
+
+
+
+Example:-
+
+Input
+
+a=3 , b=4
+
+Output
+
+a=4, b=3 */
+
+let a = 3;
+let b = 4;
+let temp = a;
+a = b;
+b = temp;
+console.log('a value is =', a);
+console.log('b value is =', b);
