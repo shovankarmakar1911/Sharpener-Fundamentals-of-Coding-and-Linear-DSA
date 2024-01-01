5afabf076b073b945da2d8e597f3eb3257b20848
shovankarmakar1911 <shovankarmakar1911@gmail.com>
2024-01-01 21:50:03 +0530

    Ternary Operators - 1

---
+++ Basic Coding/5.Ternary Operators - 1/passfail.js
@@ -0,0 +1,25 @@
+/* 
+Watch the following video on ternary operators :- Video_Link
+
+Write a program to assume a value of marks and print whether a person has failed or passed using ternary Operator.
+
+
+
+If Marks> =40 --> pass
+
+ELSE ---> fail
+
+
+
+Input:-
+
+Marks =52
+
+Ouput:-
+
+pass
+
+*/
+
+let Marks = 52;
+Marks >= 40 ? console.log("pass") : console.log("fail");
