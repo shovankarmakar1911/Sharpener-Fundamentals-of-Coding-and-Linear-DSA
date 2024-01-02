a3c1853e1981d364a374f39bbc3a9a47401917f6
shovankarmakar1911 <shovankarmakar1911@gmail.com>
2024-01-02 21:23:16 +0530

    Ternary Operators - 2

---
+++ Basic Coding/Ternary Operator -2/maxoftwoternary.js
@@ -0,0 +1,19 @@
+/*
+Write a program to print the maximum of two numbers using Ternary operator.
+
+
+
+Example:-
+
+Input:-
+
+a=3, b=5
+
+Output:-
+
+5
+*/
+
+let a = 3;
+let b = 5;
+a > b ? console.log(a) : console.log(b);
